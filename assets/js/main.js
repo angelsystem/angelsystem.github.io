23b235fe1e3a6680c06dabbe104d11cb62865d8e
Angel Rojas <angelsystem1387@gmail.com>
2023-12-21 15:04:05 -0500

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -225,20 +225,20 @@ root.style.setProperty('--textOneSteps', textOne);
 const textTwo = document.getElementById('textTwo').innerText.length;
 root.style.setProperty('--textTwoSteps', textTwo);
 
-const textThree = document.getElementById('textThree').innerText.length;
-root.style.setProperty('--textThreeSteps', textThree);
+// const textThree = document.getElementById('textThree').innerText.length;
+// root.style.setProperty('--textThreeSteps', textThree);
 
-const textFour = document.getElementById('textFour').innerText.length;
-root.style.setProperty('--textFourSteps', textFour);
+// const textFour = document.getElementById('textFour').innerText.length;
+// root.style.setProperty('--textFourSteps', textFour);
 
-const textFive = document.getElementById('textFive').innerText.length;
-root.style.setProperty('--textFiveSteps', textFive);
+// const textFive = document.getElementById('textFive').innerText.length;
+// root.style.setProperty('--textFiveSteps', textFive);
 
-const textSix = document.getElementById('textSix').innerText.length;
-root.style.setProperty('--textSixSteps', textSix);
+// const textSix = document.getElementById('textSix').innerText.length;
+// root.style.setProperty('--textSixSteps', textSix);
 
-const textSeven = document.getElementById('textSeven').innerText.length;
-root.style.setProperty('--textSevenSteps', textSeven);
+// const textSeven = document.getElementById('textSeven').innerText.length;
+// root.style.setProperty('--textSevenSteps', textSeven);
 
 let totalCallTime = 1276;
 
